af726154cb48f8465db7f0d98a057ddc667f3a31
gunvalk <gunvalk@gmail.com>
2020-06-02 17:31:41 -0400

    Update build.sh
    bork bork bork

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ cat<<EOF
     ${YELLOW}------------------------------${RESET}
     ${CYAN}Updates:
 
-    uno                                   
+    I dont feel so good mr stark                                  
 
     ${RESET}${YELLOW}------------------------------${RESET}
     ${CYAN}build.sh Update 15${RESET}
