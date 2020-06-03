51aec715aeafc6b3fbf5632136c7e98ac62dde0f
gunvalk <gunvalk@gmail.com>
2020-06-03 18:11:43 -0400

    Update build.sh
    opens build folder window when build is complete

--- build.sh
+++ build.sh
@@ -358,6 +358,8 @@ if [ "${CMDL}" != " clean" ] && [ `getconf LONG_BIT` = "64" ]; then
 		fi
     	printf "\n${GREEN}The sm64pc binary is now available in the 'build/us_pc/' folder.\n"
 		printf "\n${YELLOW}If fullscreen doesn't seem like the correct resolution, then right click on the\nexe, go to properties, compatibility, then click Change high DPI settings.\nCheck the 'Override high DPI scaling behavior' checkmark, leave it on\napplication, then press apply."
+		cd ./build/us_pc/
+		start .
 		break 2> /dev/null
 		return 2> /dev/null
 	else
